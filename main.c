884a3a733d1a2ae9332f698ef42bc74d7292d609
LEXICONAHHHH <140211763+LEXICONAHHHH@users.noreply.github.com>
2024-02-29 19:42:47 -0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,18 +1,16 @@
+
 #include <X11/Xlib.h>
-#include <unistd.h>
-#include <stdio.h>
 
 int main()
 {
-  // Open a display
-  Display* display = XOpenDisplay(0);
+    Display* display;
 
-  if (display == NULL)
-  {
-      printf("Xorg server failed to open!\n");
-      return -1;
-  }
-  
-  printf("Xorg successfully opened!\n");
-  return 0;
+    display = XOpenDisplay("simey:0");
+    
+    if (display == NULL)
+    { 
+      printf("Xorg server failed to open! \n");
+
+      return 0;
+    }
 }
